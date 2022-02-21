471b44e91fba8359a23b08b9ec601633dd84356c
Martin Novák <martin.novak@gmx.com>
2022-02-21 10:16:06 +0100

    Úprava myitem.h
    přidání komentářů, odstranění nepoužívaných proměnných, přesun některých metod z public na private

--- source/myitem.h
+++ source/myitem.h
@@ -7,32 +7,38 @@
 
 class myitem : public QGraphicsItem
 {
-    double souradnice_x;
-    double souradnice_y;
-    double zx;
-    double zy;
-    double wh;
-    Qt::GlobalColor color;
-    Qt::GlobalColor pom;
-
-    QVector<QGraphicsLineItem *> lines;
-    bool centroid;
+private:
+    double souradnice_x;                 // x-ova souradnice pro inicializaci
+    double souradnice_y;                 // y-ova souradnice pro inicializaci
+    double zx;                           // x-ova souradnice po premisteni uzivatelem na jinou pozici
+    double zy;                           // y-ova souradnice po premisteni uzivatelem na jinou pozici
+    double wh;                           // strana ctverce (item ackoli se vykresluje jen kruh tak vystupuje jako ctverec)
+    Qt::GlobalColor color;               // barva kruhu
+    QVector<QGraphicsLineItem *> lines;  // linky pripojene k itemu (centroid muze mit vice bod max 1)
+    bool centroid;                       // boolean rikajici, jestli se jedna o centroid ci ne
 public:
-    myitem(double x, double y, double wh, Qt::GlobalColor color);
+    myitem(double x, double y, double wh, Qt::GlobalColor color);   // konstruktor
 
+    //gettery a settery
+    double getX(){return zx;};            // getter na x-ovou souradnici leveho horniho rohu itemu
+    double getY(){return zy;};            // getter na y-ovou souradnici leveho horniho rohu itemu
+    double getCenterX(){return zx +wh/2;};// getter na stredovou x-ovou souradnici itemu
+    double getCenterY(){return zy +wh/2;};// getter na stredovou y-ovou souradnici itemu
+    void setX(double x){zx = x;};         // setter na stredovou x-ovou souradnici itemu
+    void setY(double y){zy = y;};         // setter na stredovou y-ovou souradnici itemu
+protected:
+//metody zabyvajici se vykreslovanim itemu
+//overridnute metody pro vykresleni itemu
     QRectF boundingRect() const override;
     void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget * widget) override;
 
-    double getX(){return zx;};
-    double getY(){return zy;};
-    double getCenterX(){return zx +wh/2;};
-    double getCenterY(){return zy +wh/2;};
-    void setX(double x){zx = x;};
-    void setY(double y){zy = y;};
-
-    void addLine(QGraphicsLineItem *line, bool centroid);
-    QVariant itemChange(GraphicsItemChange change, const QVariant &value) override;
-    void moveLineToCenter();
+//metody zabyvajici posouvanim linky soucasne s bodem
+public:
+    void addLine(QGraphicsLineItem *line, bool centroid); // metoda na spojeni linky s itemem
+protected:
+    QVariant itemChange(GraphicsItemChange change, const QVariant &value) override; //overridnuta metoda zachytavajici zmenu polohy itemu
+private:
+    void moveLineToCenter();    //metoda na zmenu polohy linek
 };
 
 #endif // MYITEM_H
