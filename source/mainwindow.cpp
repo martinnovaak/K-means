85efa3d8c111c38dc0e6bf21bea3ebe91385f29c
Martin Novák <martin.novak@gmx.com>
2022-02-23 16:43:58 +0100

    Přidání možnosti aktualizace scény při přesunu bodů
    Přidány možnosti aktualizace scény při přesouvání bodů a pouštění bodů. Přidány 3 radiobuttony pro 3 režimy aktualizací.

--- source/mainwindow.cpp
+++ source/mainwindow.cpp
@@ -5,9 +5,14 @@
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
-    , ui(new Ui::MainWindow),velikostBodu(10), sirkaPlatna(719-velikostBodu/2), vyskaPlatna(469-velikostBodu/2), pocetBodu(50), pocetCentroidu(5)
+    , ui(new Ui::MainWindow),velikostBodu(10), sirkaPlatna(719-velikostBodu/2), vyskaPlatna(469-velikostBodu/2)
+    , pocetBodu(50), pocetCentroidu(5)
 {
     ui->setupUi(this);
+
+    //zakaz aktualizovat
+    this->povolitAktualizaci = false;
+    this->rezimAktualizace = 0;
 
     scena = new QGraphicsScene(this);
     ui->graphicsView->setScene(scena);
@@ -19,12 +24,27 @@ MainWindow::MainWindow(QWidget *parent)
     connect(ui->solveButton, &QPushButton::clicked, this, &MainWindow::vyres);
     connect(ui->pointButton, &QPushButton::clicked, this, &MainWindow::vygenerujBody);
     connect(ui->centroidButton, &QPushButton::clicked, this, &MainWindow::vygenerujCentroidy);
+
+    //napojeni signalu z radiobuttonu
+    connect(ui->radioButton, &QRadioButton::clicked, this, &MainWindow::nastavAktualizace);
+    connect(ui->radioButton_2, &QRadioButton::clicked, this, &MainWindow::nastavAktualizace);
+    connect(ui->radioButton_3, &QRadioButton::clicked, this, &MainWindow::nastavAktualizace);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
-    uvolniPamet();
+
+    for(int i = 0; i < body.size(); i++)
+        delete body.takeAt(i);
+    for(int i = 0; i < centroidy.size(); i++)
+        delete centroidy.takeAt(i);
+    for(int i = 0; i < linky.size(); i++)
+        delete linky.takeAt(i);
+    for(int i = 0; i < hraniceVoronehoBunek.size(); i++)
+        delete hraniceVoronehoBunek.takeAt(i);
+    delete scena;
+
     this->body.clear();
     this->centroidy.clear();
     this->linky.clear();
@@ -51,6 +71,8 @@ void MainWindow::vygeneruj(bool p) //je-li p == true generuje body jinak centroi
         {
             el = new myitem(w, h, 10, Qt::darkYellow);
             body.push_back(el);
+            connect(this->body[i], &myitem::itemReleased, this, [this]{aktualizuj(true);});
+            connect(this->body[i], &myitem::itemChanged, this, [this]{aktualizuj(false);});
         }
         else
         {
@@ -125,20 +147,6 @@ void MainWindow::voronoi()
     delete[] y;
 }
 
-void MainWindow::uvolniPamet()
-{
-    for(int i = 0; i < body.size(); i++)
-        delete body.takeAt(i);
-    for(int i = 0; i < centroidy.size(); i++)
-        delete centroidy.takeAt(i);
-    for(int i = 0; i < linky.size(); i++)
-        delete linky.takeAt(i);
-    for(int i = 0; i < hraniceVoronehoBunek.size(); i++)
-        delete hraniceVoronehoBunek.takeAt(i);
-
-    delete scena;
-}
-
 //predela body z myitem* na bod resp. centroid
 void MainWindow::ulozBodyDoVektoru(QVector<Centroid> &c, QVector<Bod> &b)
 {
@@ -167,6 +175,7 @@ void MainWindow::ulozBodyDoVektoru(QVector<Centroid> &c, QVector<Bod> &b)
 //vygeneruje nove body (nejprve smaze ty stare a vygeneruje nove)
 void MainWindow::vygenerujBody()
 {
+    povolitAktualizaci = false; // zakaz aktualizace
     smazBody(true);
     this->pocetBodu = ui->spinBox->value();
     vygeneruj(true);
@@ -176,6 +185,7 @@ void MainWindow::vygenerujBody()
 //vygeneruje nove centroidy (nejprve smaze ty stare a vygeneruje nove)
 void MainWindow::vygenerujCentroidy()
 {
+    povolitAktualizaci = false; // zakaz aktualizace
     smazBody(false);
     this->pocetCentroidu = ui->spinBox_2->value();
     vygeneruj(false);
@@ -184,6 +194,8 @@ void MainWindow::vygenerujCentroidy()
 
 void MainWindow::vyres()
 {
+    povolitAktualizaci = true; // povol aktualizace
+
     //parametry pro inicializaci tridy kmean
     QVector<Centroid> c;
     QVector<Bod> b;
@@ -202,6 +214,7 @@ void MainWindow::vyres()
         myitem * el = new myitem(kmin(i,0), kmin(i,1), velikostBodu, Qt::red);
         centroidy.push_back(el);
         //nalezne body v aktuálním shluku
+        connect(el, &myitem::itemReleased, this, [this]{aktualizuj(true);});
         for(unsigned int j = 0; j < kmin[i].pocet; j++)
         {
             unsigned int bod = kmin[i].bodyVeShluku[j]; // index bodu ve shluku
@@ -215,4 +228,30 @@ void MainWindow::vyres()
         scena->addItem(centroidy[i]);
     }
     voronoi();
+}
+
+// slot na aktualizace je volan po emitnuti signalu z itemu
+//itemreleased je true pokud byl volan ze signalu itemReleased, byl-li volan ze signalu itemChanged je false
+void MainWindow::aktualizuj(bool itemreleased)
+{
+    if(povolitAktualizaci && rezimAktualizace ) //pokud jsou vypoctene shluky a zaroven je povoleno aktualizovat
+    {
+        if(rezimAktualizace == 2 || itemreleased)
+            vyres();
+    }
+        return;
+}
+
+//slot volan po emitnuti signalu z radiobuttonu
+void MainWindow::nastavAktualizace()
+{
+    if(ui->radioButton->isChecked())
+        this->rezimAktualizace = 1;
+    else if(ui->radioButton_2->isChecked())
+        this->rezimAktualizace = 2;
+    else
+        this->rezimAktualizace = 0;
+    //je-li povoleno aktualizovat -> zaktualizuj
+    if(rezimAktualizace && povolitAktualizaci)
+        vyres();
 }
--- source/mainwindow.h
+++ source/mainwindow.h
@@ -21,6 +21,8 @@ private slots:
     void vygenerujBody();    //slot na vygenerovani bodu
     void vygenerujCentroidy(); //slot na vygenerovani centroidu
     void vyres();             //slot na vyreseni ulohy kmeans
+    void aktualizuj(bool itemreleased); //slot reagujici na signaly z myitemu
+    void nastavAktualizace();   //slot reagujici na signaly z radiobuttonu
 private:
     Ui::MainWindow *ui;
     QGraphicsScene * scena;     //platno
@@ -37,14 +39,18 @@ private:
     unsigned int pocetBodu;      //pocet bodu
     unsigned int pocetCentroidu; //pocet centroidu
 
+    bool povolitAktualizaci;    //boolean, ve kterem je ulozeno zda-li byly vypocteny shluky
+    int rezimAktualizace;       //rezim aktualizace obsahuje cisla 0 nebo 1 nebo 2
+                                // 0 - nebude dochazet k aktulizacim
+                                // 1 - bude dochazet k aktualizacim po pusteni bodu ci centroidu
+                                // 2 - bude dochazet k aktualizacim pri presouvani bodu ( centroidu ne)
+
     void vygeneruj(bool points); //funkce na vygenerovani nahodnych pozic bodu ci centroidu (podle vstupniho parametru)
 
     void smazBody(bool points); //funkce na smazani bodu(points) a centroidu ze sceny
     void smazLinky();     //funkce na smazani linek a hranic ze sceny
 
-    void voronoi();         //funkce na vykresleni hranic voroneho bunek
-
-    void uvolniPamet();
+    void voronoi();       //funkce na vykresleni hranic voroneho bunek
 
     void ulozBodyDoVektoru(QVector<Centroid> &centroidy, QVector<Bod> &body); //ulozi body do vektoru (predela QVector<myitem*> na QVector<Bod>)
 };
